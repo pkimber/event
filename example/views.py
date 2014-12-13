688c05e10b4aedf9e8163d77bd82529c0a00d093
Patrick Kimber <code@pkimber.net>
2014-12-13 18:03:42 +0000

    calendar for home page #39

--- example/views.py
+++ example/views.py
@@ -3,7 +3,16 @@ from __future__ import unicode_literals
 
 from django.views.generic import TemplateView
 
+from event.models import Event
+
 
 class HomeView(TemplateView):
 
     template_name = 'example/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context.update(dict(
+            events=Event.objects.public_calendar(),
+        ))
+        return context
